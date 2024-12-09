0d30d428f873c3d740ce2f7cf9fb5077b4baba24
MatheusHMR <matheus.henrimr@gmail.com>
2024-12-09 00:03:55 -0300

    fix: program_help

--- views/program_help.py
+++ views/program_help.py
@@ -6,7 +6,7 @@ def programHelpWindow():
         [sg.Text('Como programar?')],
         [sg.Text("""
 Cada linha do programa deve ser composta de uma sentença lógica com 
-paranteses balanceados correatmente, vinculada a uma porta ou memória booleana.
+paranteses balanceados corretamente, vinculada a uma porta ou memória booleana.
 Exemplo: O1 = (!I1 ^ (O2 | B3))
 As portas disponíveis são definidas por: O1 até O8.
 As entradas são indicadas por: I1 até I8.
