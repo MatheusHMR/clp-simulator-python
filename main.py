57811e3afe657e3de514fb477a7085d1e1e91da5
MatheusHMR <matheus.henrimr@gmail.com>
2024-12-08 23:56:33 -0300

    feat: add tooltips

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import PySimpleGUI as sg
 from components.timer import Timer
 from components.counter import Counter
-# Remover a importação de LogicalStructure
-# from reverse_polish_notation.create_notation import reverse_polish_notation
-# from reverse_polish_notation.logical_structure import LogicalStructure
 from reverse_polish_notation import logical_structure
 from views import (
     authors,
@@ -18,34 +15,29 @@ import communication as comm
 from scan_cycle.scan_cycle import ScanCycle
 
 
-
-
-# Importar a classe ScanCycle
-# Supondo que a classe ScanCycle esteja em um arquivo chamado scan_cycle.py
-from scan_cycle.scan_cycle import ScanCycle
-
 class PLCProgrammer:
     def __init__(self):
-        # Inicialização de variáveis
-
         self.scan_cycle = ScanCycle(logical_structure=logical_structure.LogicalStructure([]))
         self.in_execution = False
         self.is_connected = False
         self.serial_port = comm.initializeSerial()
+
+        # Dicionário para armazenar os valores de preset dos timers
         self.timer_values = {f'T{i+1}': 0 for i in range(32)}
+        # Dicionário para armazenar os valores de preset dos contadores
+        self.counter_values = {f'C{i+1}': 0 for i in range(8)}
+
         self.setup_gui()
 
     def setup_gui(self):
-        sg.theme('DarkBlue14')  # Tema mais moderno
-        # Definir ícones e fontes para um visual mais profissional
+        sg.theme('DarkBlue14')
         menu_layout = [
             ['Arquivo', ['Novo      Ctrl+N', 'Abrir     Ctrl+O', 'Salvar    Ctrl+S', '---', 'Sair']],
-            ['Ferramentas', ['Compilar  F9', 'Configurar Temporizadores']],
+            ['Ferramentas', ['Compilar  F9', 'Configurar Temporizadores', 'Configurar Contadores']],
             ['CLP', ['Conectar', 'Executar  F5', 'Parar']],
             ['Ajuda', ['Sobre', 'Como Programar']],
         ]
 
-        # Barra de ferramentas com botões usando símbolos Unicode
         toolbar = [
             sg.Button('🗎 Novo', tooltip='Novo (Ctrl+N)', key='Novo'),
             sg.Button('📂 Abrir', tooltip='Abrir (Ctrl+O)', key='Abrir'),
@@ -53,17 +45,16 @@ class PLCProgrammer:
             sg.Button('🔄 Compilar', tooltip='Compilar (F9)', key='Compilar'),
             sg.Button('▶ Executar', tooltip='Executar (F5)', key='Executar'),
             sg.Button('⏹ Parar', tooltip='Parar', key='Parar'),
-            sg.Button('⚙ Configurar Temporizadores', tooltip='Configurar Temporizadores', key='Configurar Temporizadores'),
+            sg.Button('⚙ Configurar Temporizadores', tooltip='Defina o valor do timer em múltiplos de 0.1s (ex: 100 = 10s)', key='Configurar Temporizadores'),
+            sg.Button('⚙ Configurar Contadores', tooltip='Defina o valor do contador (inteiro) que ele deve contar até atingir', key='Configurar Contadores'),
             sg.Button('🔌 Conectar', tooltip='Conectar', key='Conectar'),
             sg.Button('❓ Ajuda', tooltip='Ajuda', key='Sobre'),
         ]
 
-        # Área de programação com numeração de linhas
         programming_area = sg.Frame('', [
             [sg.Multiline('', size=(80, 25), key='-PROGRAM-', font='Consolas 12', autoscroll=True, expand_x=True, expand_y=True)]
         ], relief=sg.RELIEF_SUNKEN)
 
-        # Visualização gráfica do CLP
         plc_viewer = sg.Frame('Monitoramento do CLP', [
             [sg.Text('Status de Conexão:', size=(20, 1)), sg.Text('Desconectado', key='-CONN_STATUS-', text_color='red')],
             [sg.Text('Status de Execução:', size=(20, 1)), sg.Text('Parado', key='-EXEC_STATUS-', text_color='red')],
@@ -75,7 +66,6 @@ class PLCProgrammer:
             ], expand_x=True, expand_y=True)]
         ], expand_x=True, expand_y=True)
 
-        # Layout principal
         layout = [
             [sg.Menu(menu_layout)],
             toolbar,
@@ -84,7 +74,6 @@ class PLCProgrammer:
 
         self.window = sg.Window('PLC Programmer', layout, finalize=True, resizable=True)
 
-        # Bindings dos atalhos de teclado
         self.window.bind('<Control-n>', 'Novo')
         self.window.bind('<Control-o>', 'Abrir')
         self.window.bind('<Control-s>', 'Salvar')
@@ -118,7 +107,7 @@ class PLCProgrammer:
             event, values = self.window.read(timeout=100)
             if event in (sg.WIN_CLOSED, 'Sair'):
                 break
-            elif event in ('Novo', 'Abrir', 'Salvar', 'Compilar', 'Configurar Temporizadores', 'Conectar', 'Executar', 'Parar', 'Sobre', 'Como Programar'):
+            elif event in ('Novo', 'Abrir', 'Salvar', 'Compilar', 'Configurar Temporizadores', 'Configurar Contadores', 'Conectar', 'Executar', 'Parar', 'Sobre', 'Como Programar'):
                 self.handle_menu_events(event, values)
             elif event.startswith('-IN-'):
                 self.handle_input_change(event, values)
@@ -137,6 +126,8 @@ class PLCProgrammer:
             self.compile_program(values['-PROGRAM-'])
         elif event == 'Configurar Temporizadores':
             self.configure_timers()
+        elif event == 'Configurar Contadores':
+            self.configure_counters()
         elif event == 'Conectar':
             self.connect_clp()
         elif event == 'Executar':
@@ -149,7 +140,6 @@ class PLCProgrammer:
             self.show_help()
 
     def handle_input_change(self, event, values):
-        # Atualiza o estado das entradas na scan_cycle
         index = int(event.replace('-IN-', '').replace('-', '')) - 1
         self.scan_cycle.inputs[index] = values[event]
         self.scan_cycle.memory_image_inputs[index] = values[event]
@@ -201,16 +191,17 @@ class PLCProgrammer:
         if errors:
             error.errorWindow('Erro!', '\n'.join(errors))
         else:
-            # Armazenar o programa do usuário na scan_cycle
             self.scan_cycle.user_program = program_lines
             compile_success.compileSuccessWindow()
 
     def configure_timers(self):
         timer_layout = [
-            [sg.Text(f'T{i+1}:', size=(5, 1)), sg.InputText(self.timer_values[f'T{i+1}'], key=f'T{i+1}', size=(5, 1))]
+            [sg.Text(f'T{i+1}:', size=(5, 1)), 
+             sg.InputText(self.timer_values[f'T{i+1}'], key=f'T{i+1}', size=(5, 1))]
             for i in range(32)
         ]
         layout = [
+            [sg.Text('Temporizadores contam valores inteiros em múltiplos de 0.1s', tooltip='Temporizadores contam valores inteiros em múltiplos de 0.1s')],
             [sg.Column(timer_layout, scrollable=True, vertical_scroll_only=True, size=(250, 400))],
             [sg.Button('Salvar'), sg.Button('Cancelar')]
         ]
@@ -224,11 +215,37 @@ class PLCProgrammer:
                     try:
                         self.timer_values[key] = int(values[key])
                         self.window[f'-TIMER-{key[1:]}-'].update(values[key])
-                        # Atualizar o preset do temporizador na scan_cycle
                         self.scan_cycle.timers[key].preset = int(values[key])
                     except ValueError:
                         sg.popup(f'Valor inválido para {key}. Por favor, insira um número inteiro.')
                 sg.popup('Temporizadores atualizados com sucesso.')
+                break
+        window.close()
+
+    def configure_counters(self):
+        counter_layout = [
+            [sg.Text(f'C{i+1}:', size=(5, 1)), 
+             sg.InputText(self.counter_values[f'C{i+1}'], key=f'C{i+1}', size=(5, 1))]
+            for i in range(8)
+        ]
+        layout = [
+            [sg.Text('Contadores contam valores inteiros até atingir o preset', tooltip='Contadores contam valores inteiros até atingir o preset')],
+            [sg.Column(counter_layout, scrollable=True, vertical_scroll_only=True, size=(250, 200))],
+            [sg.Button('Salvar'), sg.Button('Cancelar')]
+        ]
+        window = sg.Window('Configurar Contadores', layout, modal=True)
+        while True:
+            event, values = window.read()
+            if event in (sg.WIN_CLOSED, 'Cancelar'):
+                break
+            elif event == 'Salvar':
+                for key in self.counter_values.keys():
+                    try:
+                        self.counter_values[key] = int(values[key])
+                        self.scan_cycle.counters[key].preset = self.counter_values[key]
+                    except ValueError:
+                        sg.popup(f'Valor inválido para {key}. Por favor, insira um número inteiro.')
+                sg.popup('Contadores atualizados com sucesso.')
                 break
         window.close()
 
@@ -243,44 +260,33 @@ class PLCProgrammer:
             error.errorWindow('Erro!', 'Nenhum dispositivo foi detectado.')
 
     def run_program(self):
-        # if self.is_connected:
         self.scan_cycle.set_mode('RUN')
         self.in_execution = self.update_execution_status(True)
-        # else:
-            # error.errorWindow('Erro!', 'Nenhum dispositivo conectado.')
 
     def stop_program(self):
         self.scan_cycle.set_mode('STOP')
         self.scan_cycle.initialize_system()
-        # comm.sendLedByte(self.serial_port, b'@00000000#')
         self.update_io_values([False]*8, [False]*8, [False]*32)
         self.in_execution = self.update_execution_status(False)
         self.is_connected = self.update_connection_status(False)
 
     def show_about(self):
-        sg.popup('PLC Programmer\nVersão 1.0\nDesenvolvido por Sua Empresa', title='Sobre')
+        sg.popup('PLC Programmer\nVersão 1.0\nDesenvolvido por:\nEmanuel Carlos de Mello\nFúlvio Diniz Santos\nGilmar dos Reis Júnior\nMatheus Henrique Maia Ramos\nBaseado no trabalho de Nathan Rodrigues', title='Sobre')
 
     def show_help(self):
         program_help.programHelpWindow()
 
     def execute_cycle(self):
-        # Simulação de leitura de entradas
         inputs = [self.window[f'-IN-{i+1}-'].get() for i in range(8)]
         self.scan_cycle.inputs = inputs
         self.scan_cycle.read_inputs()
-        # Atualização dos temporizadores
         self.scan_cycle.update_timers()
-        # Processamento do programa do usuário
         self.scan_cycle.process_user_program()
-        # Atualização das saídas
         self.scan_cycle.update_outputs()
-        # Atualizar as saídas na interface
         for i, val in enumerate(self.scan_cycle.outputs):
             self.window[f'-OUT-{i+1}-'].update(value=val)
-        # Atualização das memórias booleanas
         for i, val in enumerate(self.scan_cycle.boolean_memories):
             self.window[f'-BOOL-{i+1}-'].update(value=str(val))
-        # Atualização dos temporizadores na interface
         for i in range(32):
             timer_name = f'T{i+1}'
             timer_value = self.scan_cycle.timers[timer_name].remaining_time
@@ -306,6 +312,7 @@ class PLCProgrammer:
     def update_execution_status(self, status):
         self.window['-EXEC_STATUS-'].update('Executando' if status else 'Parado', text_color='green' if status else 'red')
         return status
+
 
 if __name__ == '__main__':
     app = PLCProgrammer()
